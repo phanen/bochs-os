3fc172693d3796a1905a0ac1adbc9267f6a642b6
phanium <nxhsdu@qq.com>
2023-04-10 10:07:48 +0800

    feat: print by directly writing video memory

--- mbr.asm
+++ mbr.asm
@@ -5,9 +5,10 @@ SECTION MBR vstart=0x7c00
     mov ss, ax
     mov fs, ax
     mov sp, 0x7c00
+    mov ax, 0xb800
+    mov gs, ax
 
     ; clear the screen
-      ; ah=0x6
     mov     ax, 0x600
     mov     bx, 0x700
       ; vga mode: (0, 0) -> (79, 24)
@@ -15,24 +16,55 @@ SECTION MBR vstart=0x7c00
     mov     dx, 0x184f
     int     0x10
 
-    ; get the pos of cursor
-    mov ah, 3
-    mov bh, 0
-    int 0x10
+    ; print blink 'king'
+    mov byte [gs:0x00], 'k'
+      ; 0xa: bg is green + blink
+      ; 0x4: fg is red
+    mov byte [gs:0x01], 0xa4
 
-    ; print message
-    mov ax, message
-      ; es:bp -> start of message
-    mov bp, ax
-      ; cx: len of
-    mov cx, 11
-    mov ax, 0x1301
-      ; bh: page number
-      ; bl: font attribute
-    mov bx, 0x2
-    int 0x10
+    mov byte [gs:0x02], 'i'
+    mov byte [gs:0x03], 0xa4
+
+    mov byte [gs:0x04], 'n'
+    mov byte [gs:0x05], 0xa4
+
+    mov byte [gs:0x06], 'g'
+    mov byte [gs:0x07], 0xa4
+
+
+    mov byte [gs:0x80], 'i'
+    mov byte [gs:0x81], 0xa4
+    mov byte [gs:0x82], 'n'
+    mov byte [gs:0x83], 0xa4
+    mov byte [gs:0x84], 'g'
+    mov byte [gs:0x85], 0xa4
+    mov byte [gs:0x24], 'k'
+    mov byte [gs:0x25], 0xa4
+
+    ; 100 char pre line....
+
+    mov byte [gs:0xa0], 'x'
+    mov byte [gs:0xa1], 0xa4
+    ; overwrite x
+    mov byte [gs:0xa0], 'y'
+    mov byte [gs:0xa1], 0xa4
+
+    ; write a while word
+    mov word [gs:0x9e], 'c'
+      ; LE, overwrite 'y'
+    mov word [gs:0x9f], 0xa4
+    mov dword [gs:0xa4], 'c'
+    mov dword [gs:0xa5], 0xa4
+
+    ; 'ga' in vim to see unicode
+    ; wired encoding...
+      ; utf: 4f60, nasm: 00a0bde4
+    mov dword [gs:0x160], '你' 
+    mov byte [gs:0x161], 0xa4
+      ; 597d, 00bda5e5
+    mov dword [gs:0x162], '好'
+    mov byte [gs:0x163], 0xa4 
 
     jmp $
-    message db "hello world"
-    times 510 - ($-$$) db 0
+    times 510-($-$$) db 0
     db 0x55, 0xaa
