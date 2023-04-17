3158d4fa931610f47b9206856b1fb011c920c768
phanium <nxhsdu@qq.com>
2023-04-17 16:34:51 +0800

    feat(put_int): add then test `put_int`

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "print.h"
 int main() {
-  put_str("call put_str in kernel");
+  put_str("call put_str in kernel\n");
+  put_int(0x123abc);
+  put_char('\n');
+  put_int(0x23048);
+  put_char('\n');
   while(1);
 }
