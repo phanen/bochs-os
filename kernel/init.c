a981fb00029ab59636a5208e1f9409c5b5372863
phanium <nxhsdu@qq.com>
2023-05-16 21:53:58 +0800

    fix: timer_init at last

--- kernel/init.c
+++ kernel/init.c
@@ -8,7 +8,7 @@
 void init_all() {
    put_str("init_all\n");
    idt_init(); // init all interrupt (set idt and PIC)
-   timer_init(); // init PIT
    mem_init();
    thread_init();
+   timer_init(); // init PIT
 }
