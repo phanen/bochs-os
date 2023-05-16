e86870661ae9a57295e842cbcd6d6121081628f8
phanium <nxhsdu@qq.com>
2023-05-16 18:46:24 +0800

    feat: add and register a clock intr handler (call scheduler)

--- device/timer.c
+++ device/timer.c
@@ -1,6 +1,9 @@
 #include "timer.h"
 #include "io.h"
 #include "print.h"
+#include "interrupt.h"
+#include "thread.h"
+#include "debug.h"
 
 #define IRQ0_FREQUENCY	   100
 #define INPUT_FREQUENCY	   1193180
@@ -10,6 +13,8 @@
 #define COUNTER_MODE	   2
 #define READ_WRITE_LATCH   3
 #define PIT_CONTROL_PORT   0x43
+
+uint32_t ticks; // global total ticks
 
 // set frequency of clock (write control reg, then init counter value)
 static void frequency_set(uint8_t counter_port, \
@@ -23,10 +28,27 @@ static void frequency_set(uint8_t counter_port, \
   outb(counter_port, (uint8_t)counter_value >> 8); // then high 8
 }
 
+// clock intr handler to be registered
+static void intr_timer_handler() {
+   struct task_struct* cur_thread = running_thread();
+
+   ASSERT(cur_thread->stack_magic == 0x20021225); // check stack guard
+
+   cur_thread->elapsed_ticks++;	// increase thread-local ticks
+   ticks++;  // increase global ticks
+
+   if (cur_thread->ticks == 0) { // ticks exhausted
+      schedule();
+   } else {
+      cur_thread->ticks--;
+   }
+}
+
 // init PIT8253
 void timer_init() {
   put_str("timer_init start\n");
   // set frequency_set of clock
   frequency_set(CONTRER0_PORT, COUNTER0_NO, READ_WRITE_LATCH, COUNTER_MODE, COUNTER0_VALUE);
+  register_handler(0x20, intr_timer_handler);
   put_str("timer_init done\n");
 }
