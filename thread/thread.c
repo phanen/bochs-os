fdb078349629520d5c7a1f4888dd97431ab0a407
phanium <nxhsdu@qq.com>
2023-05-18 19:12:52 +0800

    style: format

--- thread/thread.c
+++ thread/thread.c
@@ -22,151 +22,152 @@ extern void switch_to(struct task_struct* cur, struct task_struct* next);
 
 // get the PCB of running thread
 struct task_struct* running_thread() {
-    uint32_t esp;
-    asm ("mov %%esp, %0": "=g" (esp)); // get stack top
-    // get the page start of stack top (i.e. the addr of PCB)
-    return (struct task_struct*)(esp & 0xfffff000);
+  uint32_t esp;
+  asm ("mov %%esp, %0": "=g" (esp)); // get stack top
+  // get the page start of stack top (i.e. the addr of PCB)
+  return (struct task_struct*)(esp & 0xfffff000);
 }
 
 // simply call
 static void kernel_thread(thread_func* function, void* func_arg) {
-    intr_enable(); // !!!
-    function(func_arg);
+  intr_enable(); // !!!
+  function(func_arg);
 }
 
 // init thread_stack
 void thread_create(struct task_struct* pthread, thread_func function, void* func_arg) {
-    // preserve intr stack
-    pthread->self_kstack -= sizeof(struct intr_stack);
-    // preserve thread stack
-    pthread->self_kstack -= sizeof(struct thread_stack);
+  // preserve intr stack
+  pthread->self_kstack -= sizeof(struct intr_stack);
+  // preserve thread stack
+  pthread->self_kstack -= sizeof(struct thread_stack);
 
-    struct thread_stack* kthread_stack = (struct thread_stack*)pthread->self_kstack;
-    kthread_stack->eip = kernel_thread;
-    kthread_stack->function = function;
-    kthread_stack->func_arg = func_arg;
-    kthread_stack->ebp = kthread_stack->ebx = kthread_stack->esi = kthread_stack->edi = 0;
+  struct thread_stack* kthread_stack = (struct thread_stack*)pthread->self_kstack;
+  kthread_stack->eip = kernel_thread;
+  kthread_stack->function = function;
+  kthread_stack->func_arg = func_arg;
+  kthread_stack->ebp = kthread_stack->ebx = kthread_stack->esi = kthread_stack->edi = 0;
 }
 
 // init basic info
 void init_thread(struct task_struct* pthread, char* name, int prio) {
-    memset(pthread, 0, sizeof(*pthread));
-    strcpy(pthread->name, name);
+  memset(pthread, 0, sizeof(*pthread));
+  strcpy(pthread->name, name);
 
-    if (pthread == main_thread) {
-	pthread -> status = TASK_RUNNING;
-    } else {
-	pthread->status = TASK_READY;
-    }
+  if (pthread == main_thread) {
+    pthread -> status = TASK_RUNNING;
+  } else {
+    pthread->status = TASK_READY;
+  }
 
-    pthread->priority = prio;
-    pthread->self_kstack = (uint32_t*)((uint32_t)pthread + PG_SIZE); // top of PCB page
+  pthread->priority = prio;
+  pthread->self_kstack = (uint32_t*)((uint32_t)pthread + PG_SIZE); // top of PCB page
 
-    pthread->ticks = prio;
-    pthread->elapsed_ticks = 0;
+  pthread->ticks = prio;
+  pthread->elapsed_ticks = 0;
 
-    pthread->pgdir = NULL;
+  pthread->pgdir = NULL;
 
-    pthread->stack_magic = 0x20021225; // magic number
+  pthread->stack_magic = 0x20021225; // magic number
 }
 
 // create a new thread (TCB) with provided description
 struct task_struct* thread_start(char* name, int prio, thread_func function, void* func_arg) {
-    struct task_struct* thread = get_kernel_pages(1); // PCBs (include user PCB) are all in kernel space (because we implement kernel-level thread)
-    init_thread(thread, name, prio);
-    thread_create(thread, function, func_arg); // bind function to thread
-    // asm volatile ("movl %0, %%esp; pop %%ebp; pop %%ebx; pop %%edi; pop %%esi; ret" \
-		  : : "g" (thread->self_kstack) : "memory");
+  struct task_struct* thread = get_kernel_pages(1); // PCBs (include user PCB) are all in kernel space (because we implement kernel-level thread)
+  init_thread(thread, name, prio);
+  thread_create(thread, function, func_arg); // bind function to thread
+  // asm volatile ("movl %0, %%esp; pop %%ebp; pop %%ebx; pop %%edi; pop %%esi; ret" \
+          : : "g" (thread->self_kstack) : "memory");
 
-    // ensure the thread to be created not in list now
-    ASSERT(!elem_find(&thread_ready_list, &thread->general_tag));
-    list_append(&thread_ready_list, &thread->general_tag);
+  // ensure the thread to be created not in list now
+  ASSERT(!elem_find(&thread_ready_list, &thread->general_tag));
+  list_append(&thread_ready_list, &thread->general_tag);
 
-    ASSERT(!elem_find(&thread_all_list, &thread->all_list_tag));
-    list_append(&thread_all_list, &thread->all_list_tag);
+  ASSERT(!elem_find(&thread_all_list, &thread->all_list_tag));
+  list_append(&thread_all_list, &thread->all_list_tag);
 
-    return thread;
+  return thread;
 }
 
 // to better taking kernel main as a abstract of thread
 static void make_main_thread() {
-    // main thread has been running (since `esp,0xc009f000` in loader.S)
-    // we preserve the TCB at 0xc009e000, no need to alloc page
-    main_thread = running_thread();
-    init_thread(main_thread, "main", 31);
+  // main thread has been running (since `esp,0xc009f000` in loader.S)
+  // we preserve the TCB at 0xc009e000, no need to alloc page
+  main_thread = running_thread();
+  init_thread(main_thread, "main", 31);
 
-    // only in all_list
-    ASSERT(!elem_find(&thread_all_list, &main_thread->all_list_tag));
-    list_append(&thread_all_list, &main_thread->all_list_tag);
+  // only in all_list
+  ASSERT(!elem_find(&thread_all_list, &main_thread->all_list_tag));
+  list_append(&thread_all_list, &main_thread->all_list_tag);
 }
 
 // naive round-robin
 void schedule() {
 
-    ASSERT(intr_get_status() == INTR_OFF);
+  ASSERT(intr_get_status() == INTR_OFF);
 
-    // push current thread into rdy list
-    struct task_struct* cur = running_thread();
-    if (cur->status == TASK_RUNNING) { // due to ticks exhausted
-	ASSERT(!elem_find(&thread_ready_list, &cur->general_tag));
-	list_append(&thread_ready_list, &cur->general_tag);
-	cur->ticks = cur->priority;
-	cur->status = TASK_READY;
-    } else {
-	// due to waiting event (e.g. block by IO, sync...)
-    }
+  // push current thread into rdy list
+  struct task_struct* cur = running_thread();
+  if (cur->status == TASK_RUNNING) { // due to ticks exhausted
+    ASSERT(!elem_find(&thread_ready_list, &cur->general_tag));
+    list_append(&thread_ready_list, &cur->general_tag);
+    cur->ticks = cur->priority;
+    cur->status = TASK_READY;
+  } else {
+    // due to waiting event (e.g. block by IO, sync...)
+  }
 
-    // pop ready thread from rdy list (converted from a list elem tag)
-    ASSERT(!list_empty(&thread_ready_list)); // no idle now...
-    thread_tag = NULL; // necessary? good habit?
-    thread_tag = list_pop(&thread_ready_list);
-    // to get the addr of PCB from a PCB field
-    //	though we can still do the same as in `running_thread`
-    //	why bother a new way? because we can
-    struct task_struct* next = elem2entry(struct task_struct, general_tag, thread_tag);
-    next->status = TASK_RUNNING;
+  // pop ready thread from rdy list (converted from a list elem tag)
+  ASSERT(!list_empty(&thread_ready_list)); // no idle now...
+  thread_tag = NULL; // necessary? good habit?
+  thread_tag = list_pop(&thread_ready_list);
+  // to get the addr of PCB from a PCB field
+  //	though we can still do the same as in `running_thread`
+  //	why bother a new way? because we can
+  struct task_struct* next = elem2entry(struct task_struct, general_tag, thread_tag);
+  next->status = TASK_RUNNING;
 
-    switch_to(cur, next); // written in asm
+  switch_to(cur, next); // written in asm
 }
 
 // thread block itself
 // why three kinds: block/wait/hang...
 void thread_block(enum task_status stat) {
-    ASSERT(((stat == TASK_BLOCKED) || (stat == TASK_WAITING) || (stat == TASK_HANGING)));
-    enum intr_status old_status = intr_disable();
+  ASSERT(((stat == TASK_BLOCKED) || (stat == TASK_WAITING) || (stat == TASK_HANGING)));
+  enum intr_status old_status = intr_disable();
 
-    struct task_struct* cur_thread = running_thread();
-    cur_thread->status = stat;
-    schedule(); // schedule will actually have a look on status
-    // so it will not append block thread into rdy list
+  struct task_struct* cur_thread = running_thread();
+  cur_thread->status = stat;
+  schedule(); // schedule will actually have a look on status
+  // so it will not append block thread into rdy list
 
-    // after unblock
-    intr_set_status(old_status);
+  // after unblock
+  intr_set_status(old_status);
 }
 
 // unblock given thread
+// if thread has been ready, then do nothing
 void thread_unblock(struct task_struct* pthread) {
-    ASSERT(((pthread->status == TASK_BLOCKED) || (pthread->status == TASK_WAITING) || (pthread->status == TASK_HANGING)));
-    enum intr_status old_status = intr_disable();
+  ASSERT(((pthread->status == TASK_BLOCKED) || (pthread->status == TASK_WAITING) || (pthread->status == TASK_HANGING)));
+  enum intr_status old_status = intr_disable();
 
-    if (pthread->status != TASK_READY) {
-	ASSERT(!elem_find(&thread_ready_list, &pthread->general_tag));
-	if (elem_find(&thread_ready_list, &pthread->general_tag)) {
-	    PANIC("thread_unblock: blocked thread in ready_list\n");
-	}
-	list_push(&thread_ready_list, &pthread->general_tag); // make the thread quickly get exec
-	pthread->status = TASK_READY;
+  if (pthread->status != TASK_READY) {
+    ASSERT(!elem_find(&thread_ready_list, &pthread->general_tag));
+    if (elem_find(&thread_ready_list, &pthread->general_tag)) {
+      PANIC("thread_unblock: blocked thread in ready_list\n");
     }
+    list_push(&thread_ready_list, &pthread->general_tag); // make the thread quickly get exec
+    pthread->status = TASK_READY;
+  }
 
-    intr_set_status(old_status);
+  intr_set_status(old_status);
 }
 
 // init thread (thread lists and main thread)
 void thread_init(void) {
-    put_str("thread_init start\n");
-    list_init(&thread_ready_list);
-    list_init(&thread_all_list);
-    make_main_thread();
-    put_str("thread_init done\n");
+  put_str("thread_init start\n");
+  list_init(&thread_ready_list);
+  list_init(&thread_all_list);
+  make_main_thread();
+  put_str("thread_init done\n");
 }
 
