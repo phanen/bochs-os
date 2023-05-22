f8bf99351290a9790b0231292cb05d620ab0ed98
phanium <nxhsdu@qq.com>
2023-05-22 21:07:16 +0800

    feat(mm): update header

--- kernel/memory.h
+++ kernel/memory.h
@@ -27,11 +27,14 @@ extern struct pool kernel_pool, user_pool;
 
 void mem_init(void);
 
-void* get_kernel_pages(uint32_t pg_cnt);
 void* malloc_page(enum pool_flags pf, uint32_t pg_cnt);
+void* get_kernel_pages(uint32_t pg_cnt);
+void* get_user_pages(uint32_t pg_cnt);
+void* get_a_pages(uint32_t pg_cnt);
 
 // good utilities, short but powerful
 uint32_t* pte_ptr(uint32_t vaddr);
 uint32_t* pde_ptr(uint32_t vaddr);
+uint32_t addr_v2p(uint32_t vaddr);
 
 #endif
