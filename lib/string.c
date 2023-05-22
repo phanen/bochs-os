89cfc68439a7477093bd3dcc1091b5b3b0f0c4bb
phanium <nxhsdu@qq.com>
2023-05-22 22:01:56 +0800

    fix: remove redefinition

--- lib/string.c
+++ lib/string.c
@@ -2,8 +2,6 @@
 #include "global.h"
 #include "debug.h"
 
-#define NULL  ((void *) 0)
-
 void memset(void* dst_, uint8_t value, uint32_t size) {
   ASSERT(dst_ != NULL);
   uint8_t* dst = (uint8_t*)dst_;
