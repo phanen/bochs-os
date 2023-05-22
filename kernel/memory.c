4d39e1025b94a9ecf34a086a118608d69d419da7
phanium <nxhsdu@qq.com>
2023-05-22 20:44:47 +0800

    feat(mm): add user memory alloc

--- kernel/memory.c
+++ kernel/memory.c
@@ -4,6 +4,7 @@
 #include "debug.h"
 #include "print.h"
 #include "string.h"
+#include "sync.h"
 
 #define NULL  ((void *) 0)
 
@@ -29,12 +30,13 @@ struct pool {
     struct bitmap pool_bitmap;
     uint32_t phy_addr_start;
     uint32_t pool_size; // total size
+    struct lock lock;
 };
 
 struct pool kernel_pool, user_pool; // paddr pool ofr kernel and user
 struct virtual_addr kernel_vaddr; // vaddr pool for kernel
 
-// request `pg_cnt` page from vaddr pool
+// request `pg_cnt` pages from vaddr pool
 static void* vaddr_get(enum pool_flags pf, uint32_t pg_cnt) {
     int vaddr_start = 0, bit_idx_start = -1;
     uint32_t cnt = 0;
@@ -49,8 +51,19 @@ static void* vaddr_get(enum pool_flags pf, uint32_t pg_cnt) {
         }
         // bit id to mem vaddr
         vaddr_start = kernel_vaddr.vaddr_start + bit_idx_start * PG_SIZE;
-    } else {
-        // TODO: user vaddr pool
+    }
+    else {
+        struct task_struct* cur = running_thread();
+        bit_idx_start  = bitmap_scan(&cur->userprog_vaddr.vaddr_bitmap, pg_cnt);
+        if (bit_idx_start == -1) {
+            return NULL;
+        }
+
+        while(cnt < pg_cnt) {
+            bitmap_set(&cur->userprog_vaddr.vaddr_bitmap, bit_idx_start + cnt++, 1);
+        }
+        vaddr_start = cur->userprog_vaddr.vaddr_start + bit_idx_start * PG_SIZE;
+        ASSERT((uint32_t)vaddr_start < (0xc0000000 - PG_SIZE));
     }
     return (void*)vaddr_start;
 }
@@ -149,11 +162,63 @@ void* malloc_page(enum pool_flags pf, uint32_t pg_cnt) {
 
 // kernel malloc by pages
 void* get_kernel_pages(uint32_t pg_cnt) {
+    lock_acquire(&user_pool.lock);
     void* vaddr =  malloc_page(PF_KERNEL, pg_cnt);
     if (vaddr != NULL) { // clean the page frame
         memset(vaddr, 0, pg_cnt * PG_SIZE);
     }
+    lock_release(&user_pool.lock);
     return vaddr;
+}
+
+// user malloc by pages
+void* get_user_pages(uint32_t pg_cnt) {
+   lock_acquire(&user_pool.lock);
+   void* vaddr = malloc_page(PF_USER, pg_cnt);
+    if (vaddr != NULL) { // clean the page frame
+        memset(vaddr, 0, pg_cnt * PG_SIZE);
+    }
+   memset(vaddr, 0, pg_cnt * PG_SIZE);
+   lock_release(&user_pool.lock);
+   return vaddr;
+}
+
+// malloc, but use specific vaddr
+void* get_a_page(enum pool_flags pf, uint32_t vaddr) {
+    struct pool* mem_pool = pf & PF_KERNEL ? &kernel_pool : &user_pool;
+    lock_acquire(&mem_pool->lock);
+
+    struct task_struct* cur = running_thread();
+    int32_t bit_idx = -1;
+
+    if (cur->pgdir != NULL && pf == PF_USER) {
+        bit_idx = (vaddr - cur->userprog_vaddr.vaddr_start) / PG_SIZE;
+        ASSERT(bit_idx > 0);
+        bitmap_set(&cur->userprog_vaddr.vaddr_bitmap, bit_idx, 1);
+
+    }
+    else if (cur->pgdir == NULL && pf == PF_KERNEL){
+        bit_idx = (vaddr - kernel_vaddr.vaddr_start) / PG_SIZE;
+        ASSERT(bit_idx > 0);
+        bitmap_set(&kernel_vaddr.vaddr_bitmap, bit_idx, 1);
+    } 
+    else {
+        PANIC("get_a_page: not allow kernel alloc userspace or user alloc kernelspace by get_a_page");
+    }
+
+    void* page_phyaddr = palloc(mem_pool);
+    if (page_phyaddr == NULL) {
+        return NULL;
+    }
+    page_table_add((void*)vaddr, page_phyaddr); 
+    lock_release(&mem_pool->lock);
+    return (void*)vaddr;
+}
+
+// vaddr to paddr (get its pte, then read the pte)
+uint32_t addr_v2p(uint32_t vaddr) {
+    uint32_t* pte = pte_ptr(vaddr);
+    return ((*pte & 0xfffff000) + (vaddr & 0x00000fff));
 }
 
 // init
@@ -218,6 +283,8 @@ static void mem_pool_init(uint32_t all_mem) {
     bitmap_init(&kernel_pool.pool_bitmap);
     bitmap_init(&user_pool.pool_bitmap);
 
+    lock_init(&kernel_pool.lock);
+    lock_init(&user_pool.lock);
 
     // vaddr
     kernel_vaddr.vaddr_bitmap.btmp_bytes_len = kbm_length;
