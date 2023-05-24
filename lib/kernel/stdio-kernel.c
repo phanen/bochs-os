0e27bdef6769971888aa08b91c714a6783128e0b
phanium <nxhsdu@qq.com>
2023-05-25 00:13:08 +0800

    feat(printk): yet another printf for kernel

---
+++ lib/kernel/stdio-kernel.c
@@ -0,0 +1,17 @@
+#include "stdio-kernel.h"
+#include "print.h"
+#include "stdio.h"
+#include "console.h"
+#include "global.h"
+
+#include "stdarg.h"
+
+// kernel printf, awesome
+void printk(const char* format, ...) {
+   va_list args;
+   va_start(args, format);
+   char buf[1024] = {0};
+   vsprintf(buf, format, args);
+   va_end(args);
+   console_put_str(buf);
+}
