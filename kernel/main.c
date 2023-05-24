49e7113dac9e547449b9fdd87b93b36e588f7cb2
phanium <nxhsdu@qq.com>
2023-05-24 22:34:35 +0800

    test(printf): printf %s %d %c

--- kernel/main.c
+++ kernel/main.c
@@ -29,8 +29,8 @@ int main() {
 
   intr_enable();
 
-  thread_create("k_thread_a", 31, k_thread_a, "argA ");
-  thread_create("k_thread_b", 31, k_thread_b, "argB ");
+  thread_create("k_thread_a", 31, k_thread_a, "thread_a");
+  thread_create("k_thread_b", 31, k_thread_b, "thread_b");
 
   console_put_str(" main_pid:0x");
   console_put_int(sys_getpid());
@@ -45,9 +45,7 @@ int main() {
 void k_thread_a(void* arg) {
 
   char* para = arg;
-  console_put_str(" thread_a_pid:0x");
-  console_put_int(sys_getpid());
-  console_put_char('\n');
+  printf("%s, pid:%d %c", para, sys_getpid(),'\n');
 
   while (1) {
   }
@@ -56,20 +54,14 @@ void k_thread_a(void* arg) {
 void k_thread_b(void* arg) {
 
   char* para = arg;
-  console_put_str(" thread_b_pid:0x");
-  console_put_int(sys_getpid());
-  console_put_char('\n');
-
+  printf("%s, pid:%d %c", para, sys_getpid(),'\n');
   while (1) {
   }
 }
 
 void u_prog_a(void) {
-  printf(" prog_a_pid:0x%x\n", getpid());
-
-  // console_put_str(" proc_a_pid:0x");
-  // console_put_int(bss_var_a);
-  // console_put_char('\n');
+  char* name = "uprog_a";
+  printf("%s, pid:%d %c", name, getpid(),'\n');
   while(1) {
     bss_var_a = getpid();
   }
@@ -77,10 +69,8 @@ void u_prog_a(void) {
 }
 
 void u_prog_b(void) {
-  printf(" prog_b_pid:0x%x\n", getpid());
-  // console_put_str(" proc_b_pid:0x");
-  // console_put_int(bss_var_b);
-  // console_put_char('\n');
+  char* name = "uprog_b";
+  printf("%s, pid:%d %c", name, getpid(),'\n');
   while(1) {
     bss_var_b = getpid();
   }
