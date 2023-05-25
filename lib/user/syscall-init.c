67755a7a49ce8f62718cb99189fdb620b4a086e8
phanium <nxhsdu@qq.com>
2023-05-25 11:49:20 +0800

    feat(syscall): add syscall for mm alloc and free

--- lib/user/syscall-init.c
+++ lib/user/syscall-init.c
@@ -6,6 +6,8 @@
 #include "console.h"
 #include "string.h"
 
+#include "memory.h"
+
 #define syscall_nr 32
 typedef void* syscall;
 syscall syscall_table[syscall_nr];
@@ -23,5 +25,7 @@ void syscall_init() {
   put_str("syscall_init start\n");
   syscall_table[SYS_GETPID] = sys_getpid;
   syscall_table[SYS_WRITE] = sys_write;
+  syscall_table[SYS_MALLOC] = sys_malloc;
+  syscall_table[SYS_FREE] = sys_free;
   put_str("syscall_init done\n");
 }
