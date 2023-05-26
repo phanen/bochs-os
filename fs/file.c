5a961982f03f386f6f3589858a4c60504e05c002
phanium <nxhsdu@qq.com>
2023-05-26 22:51:58 +0800

    feat(fs): implement fs's bitmap alloc and bitmap sync (init fs will load bitmap)
    bitmap alloc: alloc a index in mem bitmap, but not write to disk bitmap
    - for inode bitmap, we give index
    - for block bitmap, we give the lba of block i
      - (but it should be easy to convert back to id)
    bitmap sync: specify a bit in bitmap, then sync the block which the bit is in
    - seems horrible...

--- fs/file.c
+++ fs/file.c
@@ -44,3 +44,44 @@ int32_t pcb_fd_install(int32_t globa_fd) {
    }
    return local_fd;
 }
+
+// alloc inode, give a inode no
+int32_t inode_bitmap_alloc(struct partition* part) {
+   int32_t bit_i = bitmap_scan(&part->inode_bitmap, 1);
+   if (bit_i == -1) {
+      return -1;
+   }
+   bitmap_set(&part->inode_bitmap, bit_i, 1);
+   return bit_i;
+}
+
+// alloc blk, give a sec lba
+int32_t block_bitmap_alloc(struct partition* part) {
+   int32_t bit_i = bitmap_scan(&part->block_bitmap, 1);
+   if (bit_i == -1) {
+      return -1;
+   }
+   bitmap_set(&part->block_bitmap, bit_i, 1);
+   return (part->sb->data_start_lba + bit_i);
+}
+
+// sync the blk contains bitmap[bit_idx]
+void bitmap_sync(struct partition* part, uint32_t bit_idx, uint8_t btmp_type) {
+   uint32_t off_sec = bit_idx / BITS_PER_SECTOR;
+   uint32_t off_size = off_sec * BLOCK_SIZE;
+   uint32_t sec_lba;
+   uint8_t* bitmap_off;
+
+   switch (btmp_type) {
+      case INODE_BITMAP:
+	 sec_lba = part->sb->inode_bitmap_lba + off_sec;
+	 bitmap_off = part->inode_bitmap.bits + off_size;
+      break;
+
+      case BLOCK_BITMAP:
+	 sec_lba = part->sb->block_bitmap_lba + off_sec;
+	 bitmap_off = part->block_bitmap.bits + off_size;
+      break;
+   }
+   ide_write(part->my_disk, sec_lba, bitmap_off, 1);
+}
--- fs/file.h
+++ fs/file.h
@@ -23,8 +23,17 @@ enum std_fd {
    stderr_no
 };
 
-int32_t get_free_slot_in_global(void);
+enum bitmap_type {
+   INODE_BITMAP,
+   BLOCK_BITMAP
+};
 
+int32_t get_free_slot_in_global(void);
 int32_t pcb_fd_install(int32_t globa_fd);
+
+int32_t inode_bitmap_alloc(struct partition* part);
+int32_t block_bitmap_alloc(struct partition* part);
+void bitmap_sync(struct partition* part, uint32_t bit_idx, uint8_t btmp);
+
 
 #endif
