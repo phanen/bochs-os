a84b9b87f154b0b8684ac6936ea1a307794cb324
phanium <nxhsdu@qq.com>
2023-05-27 15:57:45 +0800

    feat(fs): impl search a absolute path (return inode)
    also give parent dir, f_type by `path_search_record`
    why not just give its dir_entry as search result?
    - `path_search_record` is a dynamic helper, or a context

--- fs/fs.h
+++ fs/fs.h
@@ -9,11 +9,20 @@
 #define SECTOR_SIZE 512
 #define BLOCK_SIZE SECTOR_SIZE	    // block = sector
 
+#define MAX_PATH_LEN 512
+
 enum file_types {
    FT_UNKNOWN,
    FT_REGULAR,
    FT_DIRECTORY
 };
+
+struct path_search_record {
+   char searched_path[MAX_PATH_LEN]; // full path searched
+   struct dir* parent_dir;
+   enum file_types file_type;
+};
+
 void fs_init(void);
 
 int32_t path_depth_cnt(char* pathname);
