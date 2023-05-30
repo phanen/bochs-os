b968e38b5ced64ef5450279b527e3d5f98052402
phanium <nxhsdu@qq.com>
2023-05-30 09:46:15 +0800

    feat(read): add `file_read`

--- fs/file.c
+++ fs/file.c
@@ -9,7 +9,6 @@
 #include "string.h"
 #include "thread.h"
 #include "global.h"
-#include <stdint.h>
 
 struct file file_table[MAX_FILE_OPEN];
 
@@ -295,7 +294,7 @@ int32_t file_write(struct file* file, const void* buf, uint32_t count) {
 	 all_blocks[blk_i] = file->fd_inode->i_sectors[blk_i];
 
 	 // alloc new block ptr, and get it
-	 
+
 	 for (blk_i = file_has_used_blocks; blk_i < file_will_use_blocks; blk_i++) {
 	    block_lba = block_bitmap_alloc(cur_part);
 	    if (block_lba == -1) {
@@ -308,7 +307,7 @@ int32_t file_write(struct file* file, const void* buf, uint32_t count) {
 	    ASSERT(file->fd_inode->i_sectors[blk_i] == 0);
 	    file->fd_inode->i_sectors[blk_i] = all_blocks[blk_i] = block_lba;
 	    block_bitmap_idx = block_lba - cur_part->sb->data_start_lba;
-	    bitmap_sync(cur_part, block_bitmap_idx, BLOCK_BITMAP); 
+	    bitmap_sync(cur_part, block_bitmap_idx, BLOCK_BITMAP);
 	 }
       }
       // need alloc new indirect table block first
@@ -325,7 +324,7 @@ int32_t file_write(struct file* file, const void* buf, uint32_t count) {
 	 ASSERT(file->fd_inode->i_sectors[12] == 0);
 	 indirect_block_table = file->fd_inode->i_sectors[12] = block_lba;
 
-	 
+
 	 for (blk_i = file_has_used_blocks; blk_i < file_will_use_blocks; blk_i++) {
 	    block_lba = block_bitmap_alloc(cur_part);
 	    if (block_lba == -1) {
@@ -337,7 +336,7 @@ int32_t file_write(struct file* file, const void* buf, uint32_t count) {
 	       ASSERT(file->fd_inode->i_sectors[blk_i] == 0);
 	       file->fd_inode->i_sectors[blk_i] = block_lba;
 	       all_blocks[blk_i] = block_lba;
-	    } 
+	    }
 	    else {
 	       all_blocks[blk_i] = block_lba;
 	    }
@@ -411,4 +410,102 @@ int32_t file_write(struct file* file, const void* buf, uint32_t count) {
    sys_free(all_blocks);
    sys_free(io_buf);
    return bytes_written;
+}
+
+// read bytes starting from fd_pos
+// return bytes_written
+int32_t file_read(struct file* file, void* buf, uint32_t count) {
+   uint8_t* buf_dst = (uint8_t*)buf;
+   uint32_t size = count, size_left = size;
+
+   // bound check
+   if ((file->fd_pos + count) > file->fd_inode->i_size)	{
+      size = file->fd_inode->i_size - file->fd_pos;
+      size_left = size;
+      if (size == 0) {
+	 return -1;
+      }
+   }
+
+   uint8_t* io_buf = sys_malloc(BLOCK_SIZE);
+   if (io_buf == NULL) {
+      printk("file_read: sys_malloc for io_buf failed\n");
+   }
+   uint32_t* all_blocks = (uint32_t*)sys_malloc(FLATTEN_PTRS * sizeof(uint32_t));
+   if (all_blocks == NULL) {
+      printk("file_read: sys_malloc for all_blocks failed\n");
+      return -1;
+   }
+
+   uint32_t block_read_start_idx = file->fd_pos / BLOCK_SIZE;
+   uint32_t block_read_end_idx = (file->fd_pos + size) / BLOCK_SIZE;
+   uint32_t read_blocks = block_read_start_idx - block_read_end_idx;
+   // TODO: 0 - 138?? 139??
+   ASSERT(block_read_start_idx < 139 && block_read_end_idx < 139);
+
+   int32_t indirect_block_table;
+   uint32_t block_idx;
+
+   if (read_blocks == 0) {
+      ASSERT(block_read_end_idx == block_read_start_idx);
+      if (block_read_end_idx < 12) {
+	 block_idx = block_read_end_idx;
+	 all_blocks[block_idx] = file->fd_inode->i_sectors[block_idx];
+      }
+      else {
+	 indirect_block_table = file->fd_inode->i_sectors[12];
+	 ide_read(cur_part->my_disk, indirect_block_table, all_blocks + 12, 1);
+      }
+   }
+      // need alloc
+   else {
+      // read only direct
+      if (block_read_end_idx < 12) {
+	 block_idx = block_read_start_idx;
+	 while (block_idx <= block_read_end_idx) {
+	    all_blocks[block_idx] = file->fd_inode->i_sectors[block_idx];
+	    block_idx++;
+	 }
+      }
+	 // read direct and need load tab
+      else if (block_read_start_idx < 12 && block_read_end_idx >= 12) {
+	 block_idx = block_read_start_idx;
+	 while (block_idx < 12) {
+	    all_blocks[block_idx] = file->fd_inode->i_sectors[block_idx];
+	    block_idx++;
+	 }
+	 ASSERT(file->fd_inode->i_sectors[12] != 0);
+
+	 indirect_block_table = file->fd_inode->i_sectors[12];
+	 ide_read(cur_part->my_disk, indirect_block_table, all_blocks + 12, 1);
+      }
+	 // need load tab
+      else {
+	 ASSERT(file->fd_inode->i_sectors[12] != 0);
+	 indirect_block_table = file->fd_inode->i_sectors[12];
+	 ide_read(cur_part->my_disk, indirect_block_table, all_blocks + 12, 1);
+      }
+   }
+
+   uint32_t sec_idx, sec_lba, sec_off_bytes, sec_left_bytes, chunk_size;
+   uint32_t bytes_read = 0;
+   while (bytes_read < size) {
+      sec_idx = file->fd_pos / BLOCK_SIZE;
+      sec_lba = all_blocks[sec_idx];
+      sec_off_bytes = file->fd_pos % BLOCK_SIZE;
+      sec_left_bytes = BLOCK_SIZE - sec_off_bytes;
+      chunk_size = size_left < sec_left_bytes ? size_left : sec_left_bytes;
+
+      memset(io_buf, 0, BLOCK_SIZE);
+      ide_read(cur_part->my_disk, sec_lba, io_buf, 1);
+      memcpy(buf_dst, io_buf + sec_off_bytes, chunk_size);
+
+      buf_dst += chunk_size;
+      file->fd_pos += chunk_size;
+      bytes_read += chunk_size;
+      size_left -= chunk_size;
+   }
+   sys_free(all_blocks);
+   sys_free(io_buf);
+   return bytes_read;
 }
