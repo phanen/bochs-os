abd60065225d0106f8c051ab2d2cd2cea991e7cd
phanium <nxhsdu@qq.com>
2023-05-31 17:49:16 +0800

    feat(syscall): register and wrap a bouch of syscall
    TODO: sys_ps

--- lib/user/syscall.h
+++ lib/user/syscall.h
@@ -3,16 +3,33 @@
 
 #include "stdint.h"
 #include "thread.h"
+#include "fs.h"
 
 enum SYSCALL_NR {
   SYS_GETPID,
-  SYS_WRITE,
   SYS_MALLOC,
   SYS_FREE,
   SYS_FORK,
-  SYS_READ,
   SYS_PUTCHAR,
   SYS_CLEAR,
+
+  SYS_OPEN,
+  SYS_CLOSE,
+  SYS_WRITE,
+  SYS_READ,
+  SYS_LSEEK,
+  SYS_UNLINK,
+
+  SYS_MKDIR,
+  SYS_OPENDIR,
+  SYS_CLOSEDIR,
+  SYS_RMDIR,
+  SYS_READDIR,
+  SYS_REWINDDIR,
+  SYS_STAT,
+  SYS_GETCWD,
+  SYS_CHDIR,
+  SYS_PS,
 };
 
 // write -> int 0x80
@@ -21,12 +38,28 @@ enum SYSCALL_NR {
 //  -> syscall_table (index = eax) -> sys_write
 
 uint32_t getpid();
-uint32_t write(int32_t fd, const void* buf, uint32_t count);
 void* malloc(uint32_t size);
 void free(void* ptr);
 pid_t fork();
-int32_t read(int32_t fd, void* buf, uint32_t count);
 void putchar(char c);
 void clear();
+
+int32_t open(char* pathname, uint8_t flag);
+int32_t close(int32_t fd);
+uint32_t write(int32_t fd, const void* buf, uint32_t count);
+int32_t read(int32_t fd, void* buf, uint32_t count);
+int32_t lseek(int32_t fd, int32_t offset, uint8_t whence);
+int32_t unlink(const char* pathname);
+
+int32_t mkdir(const char* pathname);
+struct dir* opendir(const char* name);
+int32_t closedir(struct dir* dir);
+int32_t rmdir(const char* pathname);
+struct dir_entry* readdir(struct dir* dir);
+void rewinddir(struct dir* dir);
+int32_t stat(const char* path, struct stat* buf);
+char* getcwd(char* buf, uint32_t size);
+int32_t chdir(const char* path);
+void ps(void);
 
 #endif
