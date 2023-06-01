3c2de5ce96d891693a2fb9600c1ad9053f11bb90
phanium <nxhsdu@qq.com>
2023-06-01 17:34:25 +0800

    test(load): load elf into raw disk, then `sys_write` it into fs

--- kernel/main.c
+++ kernel/main.c
@@ -20,6 +20,7 @@
 #include "shell.h"
 
 void init();
+void loadelf2fs();
 
 int main() {
 
@@ -27,12 +28,35 @@ int main() {
    init_all();
    cls_screen();
 
+   loadelf2fs();
+
    console_put_str("[phanium@bochs /]$ ");
    // shell_run();
    while (1);
    return 0;
 }
 
+void loadelf2fs() {
+   uint32_t sec_base = 300;
+   uint32_t file_size = 13888;
+   uint32_t sec_cnt = DIV_ROUND_UP(file_size, 512);
+
+   struct disk* sda = &channels[0].devices[0];
+
+   void* buf = sys_malloc(file_size);
+   ide_read(sda, sec_base, buf, sec_cnt);
+
+   int32_t fd = sys_open("/hello", O_CREAT|O_RDWR);
+   if (fd == -1) {
+      printk("open error");
+      return;
+   }
+
+   if(sys_write(fd, buf, file_size) == -1) {
+      printk("file write error!\n");
+      while(1);
+   }
+}
 
 void init(void) {
    pid_t ret_pid = fork();
