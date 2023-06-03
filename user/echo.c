88ceef0fa0a7b20c68c605b92dc86660e2f9d4bb
phanium <nxhsdu@qq.com>
2023-06-03 17:02:24 +0800

    feat: space split

--- user/echo.c
+++ user/echo.c
@@ -2,8 +2,17 @@
 #include "stdio.h"
 
 int main(int argc, char** argv) {
+  if (!(*++argv)) {
+    printf("no args!\n");
+    return -1;
+  }
 
+  printf(*argv);
   while (*++argv) {
+    printf(" ");
     printf(*argv);
   }
+  printf("\n");
+
+  return 0;
 }
